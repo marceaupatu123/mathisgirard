9e37d90d11808eb663518b01be223267ceed928e
marceaupatu123 <40667906+marceaupatu123@users.noreply.github.com>
2020-11-28 22:20:37 +0100

    try to fix clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,9 +1,9 @@
 exports.run = (client, message, args) => {
+  message.delete();
   const funtiontools = require("../main");
   let args2 = Number(args[0]);
   if (funtiontools.checkusermods(message.member) == true) {
     if (!isNaN(args2)) {
-      message.delete();
       message.channel.bulkDelete(args2).catch(console.error);
       message.channel
         .send("Message supprimé ✅")
