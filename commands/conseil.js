9b23dde14bb225ff17c7a211c4b255d1dc2c0a89
marceaupatu123 <40667906+marceaupatu123@users.noreply.github.com>
2020-11-28 17:14:57 +0100

    fixed some problems

--- commands/conseil.js
+++ commands/conseil.js
@@ -1,9 +1,13 @@
 exports.run = (client, message, args) => {
-  const funtiontools = require("../main")
-  const type = args[0];
-  const qui = functiontools.getUserFromMention(args[1]);
-  const phrase = [...args].slice(2).join(" ");
+  const functiontools = require("../main")
+  const type = args[0]
+  const qui = functiontools.getUserFromMention(args[1])
+  const phrase = args.slice(2).join(" ");
+  
 };
+
+
+
 
 module.exports.help = {
   name: "conseil",
--- main.js
+++ main.js
@@ -38,13 +38,13 @@ module.exports.checkusermods = function (user) {
 module.exports.getUserFromMention = function (mention) {
   if (!mention) return;
 
-  if (mention.startsWith("<@" && mention.endsWith(">"))) {
+  if (mention.startsWith("<@") && mention.endsWith(">")) {
     mention = mention.slice(2, -1);
 
     if (mention.startsWith("!")) {
       mention = mention.slice(1);
     }
-    return client.user.cache.get(mention)
+    return client.users.cache.get(mention)
   }
 };
 
