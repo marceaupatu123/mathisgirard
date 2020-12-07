8dfc7ebc6650d121b9b44fbfd3937c352050c206
marceaupatu123 <40667906+marceaupatu123@users.noreply.github.com>
2020-12-07 17:30:16 +0100

    add mongo

--- mongo.js
+++ mongo.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose")
 const { mongoPath } = require('./config.json')
 
 module.exports = async () => {
-    await mongoose.connect(mongoPath)
+    await mongoose.connect(mongoPath, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     return mongoose
 }
