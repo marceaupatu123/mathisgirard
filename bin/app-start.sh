e710bee7e591e5ae11887100f574b32f673dbeb5
marceaupatu123 <40667906+marceaupatu123@users.noreply.github.com>
2020-12-13 18:19:02 +0100

    abandon

--- bin/app-start.sh
+++ bin/app-start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "starting jeeves"
 cd /home/ec2-user/discord-bot
-pm2 start --icu-data-dir=node_modules/full-icu main.js --name="jeeves"
+pm2 start main.js --name="jeeves"
 echo "jeeves started"
