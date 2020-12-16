13c88360f7d3107c63cdc074a78d05b204c6c813
Dominus Marceau <marceaupatu123@icloud.com>
2020-12-16 12:52:51 +0100

    fixed ?

--- bin/app-start.sh
+++ bin/app-start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "starting jeeves"
 cd /home/ec2-user/discord-bot
-pm2 start main.js --node.args="--icu-data-dir=node_modules/full-icu" --name="jeeves"
+ pm2 start main.js -- --icu-data-dir=node_modules/full-icu
 echo "jeeves started"
--- bin/app-stop.sh
+++ bin/app-stop.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "stopping pm2"
-pm2 stop jeeves
+pm2 stop main
 echo "pm2 stopped"
