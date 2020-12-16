9ce14e9be096f98ff1f3e04d1f028a7468ef6912
Dominus Marceau <marceaupatu123@icloud.com>
2020-12-16 12:40:15 +0100

    does full icu work ?

--- bin/app-start.sh
+++ bin/app-start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "starting jeeves"
 cd /home/ec2-user/discord-bot
-pm2 start main.js --name="jeeves"
+pm2 start main.js --node.args="--icu-data-dir=node_modules/full-icu" --name="jeeves"
 echo "jeeves started"
