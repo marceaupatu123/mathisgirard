526f1dd25e13b4f689a7761cea810b1e2f7d2e90
Dominus Marceau <marceaupatu123@icloud.com>
2020-12-18 14:57:39 +0100

    eslint add

--- events/message.js
+++ events/message.js
@@ -1,15 +1,17 @@
 module.exports = async (client, message) => {
-  if (message.author.bot || !message.content.startsWith(client.config.prefix))
+  if (message.author.bot) {
     return;
+  }
+  if (message.content.startsWith(client.config.prefix)) {
+    const args = message.content
+      .slice(client.config.prefix.length)
+      .trim()
+      .split(/ +/);
+    const commande = args.shift().toLowerCase();
+    const cmd = client.commands.get(commande);
 
-  const args = message.content
-    .slice(client.config.prefix.length)
-    .trim()
-    .split(/ +/);
-  const commande = args.shift().toLowerCase();
-  const cmd = client.commands.get(commande);
+    if (!cmd) return;
 
-  if (!cmd) return;
-
-  return cmd.run(client, message, args)
+    return cmd.run(client, message, args);
+  }
 };
