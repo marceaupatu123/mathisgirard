cc338eaefc601111f4350d4dfea09df8443381a5
Dominus Marceau <marceaupatu123@icloud.com>
2020-12-18 21:19:12 +0100

    df

--- events/message.js
+++ events/message.js
@@ -5,76 +5,74 @@ const muteschema = require('../schemas/mute-schema')
 module.exports = async (client, message) => {
   if (message.author.bot) return
   let tocheck = false
-  if (!message.member.roles.cache.has(message.guild.roles.cache.find(role => role.name === 'muted'))) {
-    await mongo().then(async (mongoose) => {
+  await mongo().then(async (mongoose) => {
     // https://www.youtube.com/watch?v=A1VRitCjL6Y 10:24
-      try {
-        const guildID = message.guild.id
+    try {
+      const guildID = message.guild.id
 
-        const result = await muteschema.findOne({ _id: guildID })
-        tocheck = result.mute
-      } catch (error) {
-        tocheck = false
-      } finally {
-        mongoose.connection.close()
-      }
-    })
-    if (tocheck) {
-      if (usermap.has(message.author.id)) {
-        const userData = usermap.get(message.author.id)
-        const { lastMessage, timer } = userData
-        const difference = message.createdTimestamp - lastMessage.createdTimestamp
-        let msgCount = userData.msgCount
-        if (difference > 2000) {
-          clearTimeout(timer)
-          userData.msgCount = 1
-          userData.lastMessage = message
-          userData.timer = setTimeout(() => {
-            usermap.delete(message.author.id)
-          }, 10000)
-          usermap.set(message.author.id, userData)
-        } else {
-          ++msgCount
-          if (parseInt(msgCount) === 5) {
-            // https://www.youtube.com/watch?v=vmbhnAFzxDI 10:05
-            const muterole = await mute(client, message, message.member)
-            setTimeout(async () => {
-              message.member.roles.remove(muterole).catch(() => {
-                message.channel.send('Je n\'ai pas pu enlever le role muted.').then((msg) => {
-                  msg.delete({ timeout: 5000 })
-                })
-              })
+      const result = await muteschema.findOne({ _id: guildID })
+      tocheck = result.mute
+    } catch (error) {
+      tocheck = false
+    } finally {
+      mongoose.connection.close()
+    }
+  })
+  if (tocheck) {
+    if (usermap.has(message.author.id)) {
+      const userData = usermap.get(message.author.id)
+      const { lastMessage, timer } = userData
+      const difference = message.createdTimestamp - lastMessage.createdTimestamp
+      let msgCount = userData.msgCount
+      if (difference > 2000) {
+        clearTimeout(timer)
+        userData.msgCount = 1
+        userData.lastMessage = message
+        userData.timer = setTimeout(() => {
+          usermap.delete(message.author.id)
+        }, 10000)
+        usermap.set(message.author.id, userData)
+      } else {
+        ++msgCount
+        if (parseInt(msgCount) === 5) {
+          // https://www.youtube.com/watch?v=vmbhnAFzxDI 10:05
+          const muterole = await mute(client, message, message.member)
+          setTimeout(async () => {
+            try {
+              message.member.roles.remove(muterole)
               message.reply(`Vous avez été démute ${client.emojis.cache.get('606942836016939037')}`).then((msg) => {
                 msg.delete({ timeout: 5000 })
               })
-            }, 7000)
-          } else {
-            userData.msgCount = msgCount
-            usermap.set(message.author.id, userData)
-          }
+            } catch (err) {
+              console.log(err)
+            }
+          }, 7000)
+        } else {
+          userData.msgCount = msgCount
+          usermap.set(message.author.id, userData)
         }
-      } else {
-        const fn = setTimeout(() => {
-          usermap.delete(message.author.id)
-        }, 10000)
-        usermap.set(message.author.id, {
-          msgCount: 1,
-          lastMessage: message,
-          timer: fn
-        })
       }
+    } else {
+      const fn = setTimeout(() => {
+        usermap.delete(message.author.id)
+      }, 10000)
+      usermap.set(message.author.id, {
+        msgCount: 1,
+        lastMessage: message,
+        timer: fn
+      })
     }
-    if (message.content.startsWith(client.config.prefix)) {
-      const args = message.content
-        .slice(client.config.prefix.length)
-        .trim()
-        .split(/ +/)
-      const commande = args.shift().toLowerCase()
-      const cmd = client.commands.get(commande)
+  }
+  if (message.content.startsWith(client.config.prefix)) {
+    const args = message.content
+      .slice(client.config.prefix.length)
+      .trim()
+      .split(/ +/)
+    const commande = args.shift().toLowerCase()
+    const cmd = client.commands.get(commande)
 
-      if (!cmd) return
+    if (!cmd) return
 
-      return cmd.run(client, message, args)
-    }
+    return cmd.run(client, message, args)
   }
 }
