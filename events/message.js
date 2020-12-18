93d9bc0fc4b389301ed9b7b7f7bd57115cf742e0
Dominus Marceau <marceaupatu123@icloud.com>
2020-12-18 21:07:30 +0100

    kkk

--- events/message.js
+++ events/message.js
@@ -19,60 +19,62 @@ module.exports = async (client, message) => {
     }
   })
   if (tocheck) {
-    if (usermap.has(message.author.id)) {
-      const userData = usermap.get(message.author.id)
-      const { lastMessage, timer } = userData
-      const difference = message.createdTimestamp - lastMessage.createdTimestamp
-      let msgCount = userData.msgCount
-      if (difference > 2000) {
-        clearTimeout(timer)
-        userData.msgCount = 1
-        userData.lastMessage = message
-        userData.timer = setTimeout(() => {
-          usermap.delete(message.author.id)
-        }, 7000)
-        usermap.set(message.author.id, userData)
-      } else {
-        ++msgCount
-        if (parseInt(msgCount) === 5) {
-        // https://www.youtube.com/watch?v=vmbhnAFzxDI 10:05
-          const muterole = await mute(client, message, message.member)
-          setTimeout(async () => {
-            message.member.roles.remove(muterole).catch(() => {
-              message.channel.send('Je n\'ai pas pu enlever le role muted.').then((msg) => {
+    if (!message.guild.roles.cache.find(role => role.name === 'muted')) {
+      if (usermap.has(message.author.id)) {
+        const userData = usermap.get(message.author.id)
+        const { lastMessage, timer } = userData
+        const difference = message.createdTimestamp - lastMessage.createdTimestamp
+        let msgCount = userData.msgCount
+        if (difference > 2000) {
+          clearTimeout(timer)
+          userData.msgCount = 1
+          userData.lastMessage = message
+          userData.timer = setTimeout(() => {
+            usermap.delete(message.author.id)
+          }, 10000)
+          usermap.set(message.author.id, userData)
+        } else {
+          ++msgCount
+          if (parseInt(msgCount) === 5) {
+            // https://www.youtube.com/watch?v=vmbhnAFzxDI 10:05
+            const muterole = await mute(client, message, message.member)
+            setTimeout(async () => {
+              message.member.roles.remove(muterole).catch(() => {
+                message.channel.send('Je n\'ai pas pu enlever le role muted.').then((msg) => {
+                  msg.delete({ timeout: 5000 })
+                })
+              })
+              message.reply(`Vous avez été démute ${client.emojis.cache.get('606942836016939037')}`).then((msg) => {
                 msg.delete({ timeout: 5000 })
               })
-            })
-            message.reply(`Vous avez été démute ${client.emojis.cache.get('606942836016939037')}`).then((msg) => {
-              msg.delete({ timeout: 5000 })
-            })
-          }, 7000)
-        } else {
-          userData.msgCount = msgCount
-          usermap.set(message.author.id, userData)
+            }, 7000)
+          } else {
+            userData.msgCount = msgCount
+            usermap.set(message.author.id, userData)
+          }
         }
+      } else {
+        const fn = setTimeout(() => {
+          usermap.delete(message.author.id)
+        }, 10000)
+        usermap.set(message.author.id, {
+          msgCount: 1,
+          lastMessage: message,
+          timer: fn
+        })
       }
-    } else {
-      const fn = setTimeout(() => {
-        usermap.delete(message.author.id)
-      }, 7000)
-      usermap.set(message.author.id, {
-        msgCount: 1,
-        lastMessage: message,
-        timer: fn
-      })
     }
-  }
-  if (message.content.startsWith(client.config.prefix)) {
-    const args = message.content
-      .slice(client.config.prefix.length)
-      .trim()
-      .split(/ +/)
-    const commande = args.shift().toLowerCase()
-    const cmd = client.commands.get(commande)
+    if (message.content.startsWith(client.config.prefix)) {
+      const args = message.content
+        .slice(client.config.prefix.length)
+        .trim()
+        .split(/ +/)
+      const commande = args.shift().toLowerCase()
+      const cmd = client.commands.get(commande)
 
-    if (!cmd) return
+      if (!cmd) return
 
-    return cmd.run(client, message, args)
+      return cmd.run(client, message, args)
+    }
   }
 }
