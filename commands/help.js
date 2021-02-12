11739b771adbc21bb4c35e5142a83a7cac11c816
DOMINUS MARCEAU <marceaupatu123@gmail.com>
2021-02-12 19:47:58 +0100

    💙 Ajout de event dans le help

--- commands/help.js
+++ commands/help.js
@@ -28,7 +28,7 @@ exports.run = (client, message) => {
       {
         name: `Commandes de "${covid.name || 'La France Libre'}"`,
         value:
-          "`Conseil` : **[COMPATIBLE MENTION+]** Permet de faire un mesage à envoyer à une personne ou un channel en tant que membre d'un conseil."
+          "`Conseil` : **[COMPATIBLE MENTION+]** Permet de faire un mesage à envoyer à une personne ou un channel en tant que membre d'un conseil. \n `Event` : **Permet de lock ou delock un salon event**"
       }
     )
 
